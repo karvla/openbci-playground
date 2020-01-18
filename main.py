a30c16e3a0f3939476849fd51a848ac6fbecd0fd
Arvid Larsson <arvla626@student.liu.se>
2020-01-18 16:22:52 +0100

    Relative power withou 50 hz

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from scipy import signal
 from scipy.integrate import simps
 
 sf = 200
-t_len = 2000
+t_len = 1000
 t = np.arange(t_len) / sf
 
 def periodogram(u, t, n_win = 4):
@@ -27,17 +27,20 @@ def relative_power(a, b, freqs, psd):
     Computes the relative power of the signal between a hz and b hz.
     """
     dx = freqs[1] - freqs[0]
+
     index = np.logical_and(freqs >= a, freqs <= b)
+    index_50_hz = np.logical_and(freqs >= 45, freqs <= 55)
+
     ab_power = simps(psd[index], dx=dx)
-    total_power = simps(psd, dx=dx)
+    total_power = simps(psd, dx=dx) - simps(psd[index_50_hz], dx=dx)
     return ab_power / total_power
 
 
 def main():
-    r = Reader("C4:A9:D2:20:3F:A1")
+    r = Reader("e0:06:15:36:28:44")
     r.start()
     plot = Plotter()
-    [plot.set_properties(n, x_label = 'f', y_label = 'V^2/Hz', x_lim = (0, 60)) for n in range(4)]
+    [plot.set_properties(n, x_label = 'f', y_label = 'V^2/Hz', x_lim = (0, 50)) for n in range(4)]
 
     while len(r.channels[0]) < t_len:
         sleep(0.1)
@@ -53,7 +56,7 @@ def main():
             print(rel_power, " ", end="")
         print()
         plot.update_plot()
-        sleep(0.01)
+        sleep(0.2)
 
 
 if __name__ == "__main__":
