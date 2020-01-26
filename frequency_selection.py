9928ec43f8ff719dca6955c4605a1a3bf35da84d
Arvid Larsson <arvla626@student.liu.se>
2020-01-26 15:30:41 +0100

    Frequency selection by power

--- frequency_selection.py
+++ frequency_selection.py
@@ -4,10 +4,11 @@ import numpy as np
 def freq_peaks(freqs, power, n=1):
     """ Return the top n peaks from the freqs and power. """
     power_dt = np.gradient(power, freqs)
-    freq_power_dpdf = sorted(zip(freqs, power, power_dt), key=lambda x: abs(x[2]))
+    peaks = list(filter(lambda x: abs(x[2]) < 0.05, zip(freqs, power, power_dt)))
+    peaks_by_power = sorted(peaks, key=lambda x: abs(x[1]), reverse=True)
 
-    peaks = [(f, p) for f, p, _ in freq_power_dpdf]
-    return peaks[:n]
+    peaks = [(f, p) for f, p, _ in peaks_by_power[:n]]
+    return peaks
 
 
 if __name__ == "__main__":
