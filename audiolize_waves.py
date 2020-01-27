680f7f62512929e4ba36e852718cd8d36e14f5b9
korvlax <rickardandersson1993@gmail.com>
2020-01-27 21:00:14 +0100

    created a audiolizer from the peak freqs, each instance of peaks is played as a short 0.1 second wave

---
+++ audiolize_waves.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from time import sleep, time
+from reader import Reader
+from main import periodogram
+from frequency_selection import freq_peaks
+from synthesizer import Synth
+
+SF = 200
+T_LEN = 1000
+
+
+def map_peaks(peaks):
+    """ Maps peaks to be in an audible range """
+    return [20 + 30*peak for peak in peaks]
+
+def audiolize():
+
+    synth = Synth()
+    r = Reader(mac="e0:06:15:36:28:44", interface="udp")
+    r.start()
+    t = np.arange(T_LEN) / SF
+
+    while len(r.channels[0]) < T_LEN:
+        sleep(0.1)
+
+    while True:
+        u = r.channels[0][-T_LEN:]
+        freqs, psd = periodogram(u, t)
+        peaks, _ = zip(*freq_peaks(freqs, psd, n=3))
+        peaks = map_peaks(peaks)
+
+        waves = synth.sound_waves(peaks)
+        signal = synth.harmonize(waves)
+        synth.play_signal(volume=0.5, signal=signal)
+        sleep(synth.duration)
+
+
+if __name__ == "__main__":
+    audiolize()
--- synthesizer.py
+++ synthesizer.py
@@ -1,54 +1,48 @@
 import pyaudio
 import numpy as np
+
 from time import sleep
 from functools import reduce
-import sys
+from frequency_selection import freq_peaks
 
 
-VOLUME = 0.5     # range [0.0, 1.0]
-FS = 44100       # sampling rate, Hz, must be integer
-DURATION = 1.0   # in seconds, may be float
-BLUES_SCALE_RATIOS = np.array([1, 6/5, 4/3, 45/32, 3/2, 9/5, 2])
+class Synth:
 
+    def __init__(self):
+        self.duration = 0.1
+        self.sf = 44100
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=pyaudio.paFloat32,
+                        channels=1,
+                        rate=self.sf,
+                        output=True)
 
-def harmonize(waves):
-    """ Returns a signal consisting of a sum of sine waves. """
-    signal = list(reduce(lambda x, y : x + y, waves))
-    signal = np.array(signal)
-    return signal.astype(np.float32)
+    def harmonize(self,waves):
+        """ Returns a signal consisting of a sum of sine waves. """
+        signal = list(reduce(lambda x, y : x + y, waves))
+        signal = np.array(signal)
+        return signal.astype(np.float32)
 
-def get_waves(freqs):
-    " Returns a numpy array with containing waves with different frequencies. "
-    t = np.arange(FS*DURATION) / FS
-    sine = lambda f : np.sin(2 * np.pi * f * t)
-    waves = list(map(sine, freqs))
-    return np.array(waves)
+    def sound_waves(self, freqs):
+        " Returns a numpy array with containing waves with different frequencies. "
+        t = np.arange(self.sf*self.duration) / self.sf
+        sine = lambda f : np.sin(2 * np.pi * f * t)
+        waves = list(map(sine, freqs))
+        return np.array(waves)
 
-def blues_freqs(start_freq):
-    """ Returns a blues scale in the key corresponding to start_freq. """
-    return BLUES_SCALE_RATIOS * start_freq
-    
+    def blues_freqs(self, start_freq):
+        """ Returns a blues scale in the key corresponding to start_freq. """
+        blues_scale_ratio = np.array([1, 6/5, 4/3, 45/32, 3/2, 9/5, 2])
+        return blues_scale_ratio * start_freq
+        
+    def play_signal(self, volume, signal):
+        self.stream.write(volume * signal)
 
-def playback():
-    """ Reads waves and plays them. Quite limited atm... """
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paFloat32,
-                    channels=1,
-                    rate=FS,
-                    output=True)
+    def __del__(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
 
-    while True:
-        freqs = blues_freqs(440.0)
-        waves = get_waves(freqs)
-        signal = harmonize(waves)
-
-        stream.write(VOLUME * signal)
-        sleep(DURATION)
-
-    stream.stop_stream()
-    stream.close()
-
-    p.terminate()
-
+        
 if __name__ == "__main__":
     playback()
