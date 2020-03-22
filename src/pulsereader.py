83a45ff9ec236ac04230fee8316aa1a87e2a02a1
Orvar Korvarsson <rickardandersson1993@gmail.com>
2020-03-22 21:51:51 +0100

    Heartbeat module now runs

--- src/pulsereader.py
+++ src/pulsereader.py
@@ -1,12 +1,14 @@
 from time import sleep
 from serial import Serial
 import threading
+from synthesizer import Synth
 
 class Heartbeat(threading.Thread):
     def __init__(self, dev, synth, baudrate=9600):
         super(Heartbeat, self).__init__()
         self.reader = Serial(dev, 9600)
         self.synth = synth
+        self.data = []
 
     def run(self):
         while True:
@@ -14,10 +16,11 @@ class Heartbeat(threading.Thread):
             self.synth.play_heartbeat()
 
     def await_heartbeat(self):
-        self.reader.readline()
+        line = self.reader.readline()
+        print(int(line))
         return True
 
 if __name__ == '__main__':
-    while True:
-        print(int(ser.readline()))
-        sleep(0.05)
+    synth = Synth()
+    hb = Heartbeat('/dev/cu.usbmodem1421',synth)
+    hb.start()
